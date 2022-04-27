89a2b0f4cd07927e069b94c3b71b17bb5023e16d
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-26 20:23:34 -0500

    Refactoring class Reader and added test suit

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require('../../../lib/utils/Reader');
+
+describe('Reader class test suit', () => {
+    test('El archivo se esta leyendo correctamente', () => {
+        const explorers = Reader.readJsonFile('./explorers.json');
+        expect(explorers).not.toBe(undefined);
+    });
+});
