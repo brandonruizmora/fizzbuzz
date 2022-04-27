49ba0dbd7370e9000c33023c881eb496fe6917c2
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-26 21:28:40 -0500

    update test to pass

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -3,6 +3,6 @@ const Reader = require('../../../lib/utils/Reader');
 describe('Reader class test suit', () => {
     test('El archivo se esta leyendo correctamente', () => {
         const explorers = Reader.readJsonFile('./explorers.json');
-        expect(explorers).toBe(undefined);
+        expect(explorers).not.toBe(undefined);
     });
 });
