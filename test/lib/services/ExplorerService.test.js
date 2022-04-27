e89de7222cac62e3b4b923e3e870bb1f50e7ae63
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-26 21:48:36 -0500

    Added linter and delete app.js

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -1,36 +1,36 @@
-const ExplorerService = require('../../../lib/services/ExplorerService');
+const ExplorerService = require("../../../lib/services/ExplorerService");
 
-describe('ExplorerService class test suit', () => {
-    test('Se filtra los explorers por la mision', () => {
+describe("ExplorerService class test suit", () => {
+    test("Se filtra los explorers por la mision", () => {
         const explorers = [
-            { githubUsername: 'brandonruizmora', mission: 'node' },
-            { githubUsername: 'carlo', mission: 'node' },
-            { githubUsername: 'ferch', mission: 'java' },
+            { githubUsername: "brandonruizmora", mission: "node" },
+            { githubUsername: "carlo", mission: "node" },
+            { githubUsername: "ferch", mission: "java" },
         ];
-        const filteredExplorers = ExplorerService.filterByMission(explorers, 'node');
+        const filteredExplorers = ExplorerService.filterByMission(explorers, "node");
         expect(filteredExplorers.length).toBe(2);
         expect(filteredExplorers).toEqual([
-            { githubUsername: 'brandonruizmora', mission: 'node' },
-            { githubUsername: 'carlo', mission: 'node' },
+            { githubUsername: "brandonruizmora", mission: "node" },
+            { githubUsername: "carlo", mission: "node" },
         ]);
     });
-    test('Se obtiene la cantidad de explorers por mision', () => {
+    test("Se obtiene la cantidad de explorers por mision", () => {
         const explorers = [
-            { githubUsername: 'brandonruizmora', mission: 'node' },
-            { githubUsername: 'carlo', mission: 'node' },
-            { githubUsername: 'ferch', mission: 'java' },
+            { githubUsername: "brandonruizmora", mission: "node" },
+            { githubUsername: "carlo", mission: "node" },
+            { githubUsername: "ferch", mission: "java" },
         ];
         const amount = ExplorerService.getAmountOfExplorersByMission(explorers, "java");
         expect(amount).toBe(1);
     });
-    test('Se obtiene una lista de los nombres de usuarios por la mision', () => {
+    test("Se obtiene una lista de los nombres de usuarios por la mision", () => {
         const explorers = [
-            { githubUsername: 'brandonruizmora', mission: 'node' },
-            { githubUsername: 'carlo', mission: 'node' },
-            { githubUsername: 'ferch', mission: 'java' },
+            { githubUsername: "brandonruizmora", mission: "node" },
+            { githubUsername: "carlo", mission: "node" },
+            { githubUsername: "ferch", mission: "java" },
         ];
         const explorersUsername = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
         expect(explorersUsername.length).toBe(2);
-        expect(explorersUsername[1]).toBe('carlo');
+        expect(explorersUsername[1]).toBe("carlo");
     });
 });
--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -1,8 +1,8 @@
-const Reader = require('../../../lib/utils/Reader');
+const Reader = require("../../../lib/utils/Reader");
 
-describe('Reader class test suit', () => {
-    test('El archivo se esta leyendo correctamente', () => {
-        const explorers = Reader.readJsonFile('./explorers.json');
+describe("Reader class test suit", () => {
+    test("El archivo se esta leyendo correctamente", () => {
+        const explorers = Reader.readJsonFile("./explorers.json");
         expect(explorers).not.toBe(undefined);
     });
 });
