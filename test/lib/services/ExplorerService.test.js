3c5a941f2d1030e542ff3f95a85d3a81eba2db7c
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-26 20:54:39 -0500

    Refactoring ExplorerService and added test suit

---
+++ test/lib/services/ExplorerService.test.js
@@ -0,0 +1,36 @@
+const ExplorerService = require('../../../lib/services/ExplorerService');
+
+describe('ExplorerService class test suit', () => {
+    test('Se filtra los explorers por la mision', () => {
+        const explorers = [
+            { githubUsername: 'brandonruizmora', mission: 'node' },
+            { githubUsername: 'carlo', mission: 'node' },
+            { githubUsername: 'ferch', mission: 'java' },
+        ];
+        const filteredExplorers = ExplorerService.filterByMission(explorers, 'node');
+        expect(filteredExplorers.length).toBe(2);
+        expect(filteredExplorers).toEqual([
+            { githubUsername: 'brandonruizmora', mission: 'node' },
+            { githubUsername: 'carlo', mission: 'node' },
+        ]);
+    });
+    test('Se obtiene la cantidad de explorers por mision', () => {
+        const explorers = [
+            { githubUsername: 'brandonruizmora', mission: 'node' },
+            { githubUsername: 'carlo', mission: 'node' },
+            { githubUsername: 'ferch', mission: 'java' },
+        ];
+        const amount = ExplorerService.getAmountOfExplorersByMission(explorers, "java");
+        expect(amount).toBe(1);
+    });
+    test('Se obtiene una lista de los nombres de usuarios por la mision', () => {
+        const explorers = [
+            { githubUsername: 'brandonruizmora', mission: 'node' },
+            { githubUsername: 'carlo', mission: 'node' },
+            { githubUsername: 'ferch', mission: 'java' },
+        ];
+        const explorersUsername = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(explorersUsername.length).toBe(2);
+        expect(explorersUsername[1]).toBe('carlo');
+    });
+});
