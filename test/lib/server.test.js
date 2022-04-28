ef940ceca1a7ed6544df110ba85851b01f33fd7b
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-27 20:53:00 -0500

    added linter-fix

--- test/lib/server.test.js
+++ test/lib/server.test.js
@@ -1,29 +1,29 @@
-const request = require('supertest');
+const request = require("supertest");
 const {app, handler} = require("../../lib/server");
 
-describe('Server test suite', () => {
-    test(' / gives welcome message', async () => {
+describe("Server test suite", () => {
+    test(" / gives welcome message", async () => {
         const response = await request(app).get("/");
         expect(response.body.message).toEqual("FizzBuzz Api welcome!");
         expect(response.statusCode).toBe(200);
         expect.assertions(2);
         handler();
     });
-    test('/v1/explorers/node returns 10 explorers', async () => {
+    test("/v1/explorers/node returns 10 explorers", async () => {
         const response = await request(app).get("/v1/explorers/node");
         expect(response.body.length).toBe(10);
         expect(response.statusCode).toBe(200);
         expect.assertions(2);
         handler();
     });
-    test('/v1/explorers/amount/java returns 5 explorers', async () => {
+    test("/v1/explorers/amount/java returns 5 explorers", async () => {
         const response = await request(app).get("/v1/explorers/amount/java");
         expect(response.body.quantity).toBe(5);
         expect(response.statusCode).toBe(200);
         expect.assertions(2);
         handler();
     });
-    test('/v1/explorers/usernames/node returns 10 explorers', async () => {
+    test("/v1/explorers/usernames/node returns 10 explorers", async () => {
         const response = await request(app).get("/v1/explorers/usernames/node");
         expect(response.body.mission).toBe("node");
         expect(response.body.explorers.length).toBe(10);
