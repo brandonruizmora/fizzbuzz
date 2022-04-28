a5b5de5e4429b1585662dc28be07b67cdf10cbd4
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-27 20:30:01 -0500

    Added endpoint getExplorersInMission

--- test/lib/server.test.js
+++ test/lib/server.test.js
@@ -9,4 +9,11 @@ describe('Server test suite', () => {
         expect.assertions(2);
         handler();
     });
+    test('/v1/explorers/node returns 10 explorers', async () => {
+        const response = await request(app).get("/v1/explorers/node");
+        expect(response.body.length).toBe(10);
+        expect(response.statusCode).toBe(200);
+        expect.assertions(2);
+        handler();
+    });
 });
