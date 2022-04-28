b45d9c07218b78ecb75a2074e04df4d9a72ea31a
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-27 20:20:13 -0500

    Added test suit for server api

---
+++ test/lib/server.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const {app, handler} = require("../../lib/server");
+
+describe('Server test suite', () => {
+    test(' / gives welcome message', async () => {
+        const response = await request(app).get("/");
+        expect(response.body.message).toEqual("FizzBuzz Api welcome!");
+        expect(response.statusCode).toBe(200);
+        expect.assertions(2);
+        handler();
+    });
+});
